f8c05f5897968ca97ac6bf015cbb35b341ce84e6
S.J.R. van Schaik <stephan@synkhronix.com>
2022-02-23 16:38:46 -0500

    examples: add example of how to map anonymous memory

---
+++ examples/map_anon.rs
@@ -0,0 +1,19 @@
+use mmap_rs::{Error, MmapFlags, MmapOptions};
+
+fn main() -> Result<(), Error> {
+    // Allocate a single page of anonymous memory that is private and mutable.
+    let mut mapping = MmapOptions::new(MmapOptions::page_size().0)
+        .with_flags(MmapFlags::COPY_ON_WRITE)
+        .map_mut()?;
+
+    mapping[0..4].copy_from_slice(b"test");
+
+    let mapping = match mapping.make_read_only() {
+        Ok(mapping) => mapping,
+        Err((_, e)) => return Err(e),
+    };
+
+    println!("mapping: {:x?}", &mapping[0..4]);
+        
+    Ok(())
+}
