15bae26a68f0080e6f3bd5a29e4a2d808cde0bbf
S.J.R. van Schaik <stephan@synkhronix.com>
2023-06-07 23:44:09 -0400

    Implement test cases for mapping memory

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,45 @@ mod os_impl;
 pub use areas::*;
 pub use error::Error;
 pub use mmap::*;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn map_none() {
+        use crate::MmapOptions;
+
+        let mapping = MmapOptions::new(MmapOptions::page_size())
+            .unwrap()
+            .map_none()
+            .unwrap();
+
+        assert!(mapping.as_ptr() != std::ptr::null());
+    }
+
+    #[test]
+    fn map() {
+        use crate::MmapOptions;
+
+        let mapping = MmapOptions::new(MmapOptions::page_size())
+            .unwrap()
+            .map()
+            .unwrap();
+
+        assert!(mapping.as_ptr() != std::ptr::null());
+    }
+
+    #[test]
+    fn map_mut() {
+        use crate::MmapOptions;
+
+        let mut mapping = MmapOptions::new(MmapOptions::page_size())
+            .unwrap()
+            .map_mut()
+            .unwrap();
+
+        mapping[0] = 0x42;
+
+        assert!(mapping.as_ptr() != std::ptr::null());
+        assert_eq!(mapping[0], 0x42);
+    }
+}
